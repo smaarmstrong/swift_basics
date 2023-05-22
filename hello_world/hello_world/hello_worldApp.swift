d01335737b4027151319e8f25012a1b30834e01e
smaarmstrong <seanmaarmstrong@gmail.com>
2023-05-22 04:38:45 -0700

    hello world app project

---
+++ hello_world/hello_world/hello_worldApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_worldApp.swift
+//  hello_world
+//
+//  Created by Sean Armstrong on 5/22/23.
+//
+
+import SwiftUI
+
+@main
+struct hello_worldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ variables_constants.swift
@@ -0,0 +1,10 @@
+//
+//  variables_constants.swift
+//  
+//
+//  Created by Sean Armstrong on 5/22/23.
+//
+
+import Foundation
+
+print("Hello World")
